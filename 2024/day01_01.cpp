04be1747c663cca5caf28165f80ea6ce768c6a7c
Adam Oleksy <adam.oleksy@gmail.com>
2024-12-01 21:23:59 +0100

    Add generic .clang-format and .clang-tidy

--- 2024/day01_01.cpp
+++ 2024/day01_01.cpp
@@ -5,17 +5,18 @@
 #include <ranges>
 #include <vector>
 
-auto read_file(const std::string& filename)
+auto read_file(const std::string &filename)
 {
     std::ifstream file(filename);
     std::vector<int> a, b;
-    for(int x, y; file >> x >> y;)
+    for (int x, y; file >> x >> y;)
     {
         a.push_back(x);
         b.push_back(y);
     }
 
-    return std::make_pair(std::move(a), std::move(b));}
+    return std::make_pair(std::move(a), std::move(b));
+}
 
 int main()
 {
@@ -25,7 +26,7 @@ int main()
     std::ranges::sort(a);
     std::ranges::sort(b);
 
-    const auto sum_View = std::views::zip_transform([](auto x, auto y){ return std::abs(x-y); }, a, b);
+    const auto sum_View = std::views::zip_transform([](auto x, auto y) { return std::abs(x - y); }, a, b);
 
     std::cout << std::reduce(sum_View.begin(), sum_View.end()) << '\n';
 
--- 2024/day01_02.cpp
+++ 2024/day01_02.cpp
@@ -7,23 +7,24 @@
 #include <ranges>
 #include <vector>
 
-auto read_file(const std::string& filename)
+auto read_file(const std::string &filename)
 {
     std::ifstream file(filename);
     std::vector<std::size_t> a, b;
-    for(int x, y; file >> x >> y;)
+    for (int x, y; file >> x >> y;)
     {
         a.push_back(x);
         b.push_back(y);
     }
 
-    return std::make_pair(std::move(a), std::move(b));}
+    return std::make_pair(std::move(a), std::move(b));
+}
 
 int main()
 {
     auto [a, b] = read_file("day01.txt");
 
-    for (auto& id: a)
+    for (auto &id : a)
         id *= std::ranges::count(b, id);
 
     std::puts(std::format("{}", std::reduce(a.begin(), a.end())).c_str());
