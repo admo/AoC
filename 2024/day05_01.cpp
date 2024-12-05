8808823a2af12a3c46a534033ce3c94659d56a78
Adam Oleksy <adam.oleksy@gmail.com>
2024-12-05 21:44:10 +0100

    Use more ranges

--- 2024/day05_01.cpp
+++ 2024/day05_01.cpp
@@ -11,28 +11,35 @@
 using Rules = std::unordered_map<std::size_t, std::set<std::size_t>>;
 using Updates = std::vector<std::vector<std::size_t>>;
 
+using std::back_inserter;
+using std::istream_iterator;
+using std::size_t;
+using std::ranges::copy;
+using std::ranges::fold_left;
+using std::ranges::replace;
+using std::views::filter;
+using std::views::transform;
+
 auto read_file(const std::string &filename)
 {
     auto file = std::ifstream{filename};
+
     auto rules = Rules{};
+    auto updates = Updates{};
 
     for (std::string line; std::getline(file, line) && !line.empty();)
     {
-        std::ranges::replace(line, '|', ' ');
-        std::istringstream iss{line};
-        std::size_t page1, page2;
-        iss >> page1 >> page2;
+        replace(line, '|', ' ');
+        size_t page1, page2;
+        std::istringstream{line} >> page1 >> page2;
         rules[page1].insert(page2);
     }
 
-    auto updates = Updates{};
-
     for (std::string line; std::getline(file, line);)
     {
-        std::ranges::replace(line, ',', ' ');
-        std::istringstream iss{line};
-        std::ranges::copy(std::istream_iterator<std::size_t>{iss}, std::istream_iterator<std::size_t>{},
-                          std::back_inserter(updates.emplace_back()));
+        replace(line, ',', ' ');
+        auto iss = std::istringstream{line};
+        copy(istream_iterator<size_t>{iss}, istream_iterator<size_t>{}, back_inserter(updates.emplace_back()));
     }
 
     return std::make_pair(std::move(rules), std::move(updates));
@@ -40,16 +47,11 @@ auto read_file(const std::string &filename)
 
 auto solve(const Rules &rules, const Updates &updates)
 {
-    auto result = std::size_t{};
-    auto predicate = [&rules](const auto a, const auto b) { return !rules.contains(a) || rules.at(a).contains(b); };
+    auto is_in_order = [&rules](const auto a, const auto b) { return !rules.contains(a) || rules.at(a).contains(b); };
+    auto is_valid = [&is_in_order](const auto &update) { return std::ranges::is_sorted(update, is_in_order); };
+    auto get_middle = [](const auto &update) { return update[update.size() / 2]; };
 
-    for (const auto &update : updates)
-    {
-        const auto is_correct = std::ranges::is_sorted(update, predicate);
-        result += is_correct ? update[update.size() / 2] : 0;
-    }
-
-    return result;
+    return fold_left(updates | filter(is_valid) | transform(get_middle), size_t{}, std::plus<>{});
 }
 
 int main()
